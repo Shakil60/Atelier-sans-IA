1ce6440c560f7b20b723a910bc97c92955c98814
Shakil <khaldishakil@gmail.com>
2025-10-15 16:39:45 +0200

    fix exo 6

--- exo6.go
+++ exo6.go
@@ -1,5 +1,25 @@
 package ateliersansia
 
-func FindPackage(number int, startWagon *Wagon) ResultSearch {
+type Package struct {
+    Number    int
+    Recipient string
+    Address   string
+    Content   string
+    Weight    float64
+}
 
+type Wagon struct {
+    Number    string
+    Loading   []Package
+    NextWagon *Wagon
+}
+
+type ResultSearch struct {
+    NumberWagon     string
+    PositionPackage int
+    PackageFind     Package
+}
+
+func FindPackage(number int, startWagon *Wagon) ResultSearch {
+	return
 }
